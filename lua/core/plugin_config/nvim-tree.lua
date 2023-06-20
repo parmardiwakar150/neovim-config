818bb1a713f832a117903a2afaf5a6a85feb46b3
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-06-20 13:18:45 +0530

    hide dotfiles

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,5 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    filters = {
+        dotfiles = true,
+    },
+})
 vim.keymap.set('n', '<C-n>', ':NvimTreeFindFileToggle<CR>')

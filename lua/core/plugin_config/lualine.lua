f0640e31b604066e6959268814d56910b4dda0e5
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-06-21 23:32:22 +0530

    disable lualine for NvimTree

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -24,8 +24,7 @@ require('lualine').setup {
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
-            statusline = {},
-            winbar = {},
+            'NvimTree'
         },
         ignore_focus = {},
         always_divide_middle = true,

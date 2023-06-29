d1ac797fb783abf18039c98e926d3270da315a01
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-06-29 14:06:00 +0530

    added keymap for moving to the start of current context

--- lua/core/plugin_config/indent-blankline.lua
+++ lua/core/plugin_config/indent-blankline.lua
@@ -3,3 +3,15 @@ require("indent_blankline").setup {
     show_current_context = true,
     -- show_current_context_start = true,
 }
+
+vim.keymap.set("n", "[c", function()
+    local ok, start = require("indent_blankline.utils").get_current_context(
+        vim.g.indent_blankline_context_patterns,
+        vim.g.indent_blankline_use_treesitter_scope
+    )
+    if not ok then
+        return
+    end
+    vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start, 0 })
+    vim.cmd [[normal! _]]
+end)

a69ef247017516cb3bc555c369f3da69a4003adc
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-06-29 21:32:59 +0530

    show most recently used buffers and ignore current buffer

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,7 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+vim.keymap.set('n', '<C-b>',
+    ":lua require('telescope.builtin').buffers({ sort_mru = true, ignore_current_buffer = true })<CR>", {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>h', builtin.oldfiles)
 vim.keymap.set('n', '<leader>:', builtin.command_history)

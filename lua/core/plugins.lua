b6484d6fa9c6d011b5cdc37fe412e9fff96a3cb0
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-07-01 09:22:33 +0530

    remove delay for git blame

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -81,7 +81,16 @@ return require('packer').startup(function(use)
     use {
         'lewis6991/gitsigns.nvim',
         config = function()
-            require('gitsigns').setup()
+            require('gitsigns').setup(
+                {
+                    current_line_blame_opts = {
+                        virt_text = true,
+                        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                        delay = 0,
+                        ignore_whitespace = false,
+                    },
+                }
+            )
         end
     }
     use 'fedepujol/move.nvim'

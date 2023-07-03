15277ecf70685838b43b16295dfc4c5e76e04a6c
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-07-04 00:39:42 +0530

    source lua files only

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,8 @@
+local source_current_file = function()
+	if vim.bo.filetype == "lua" then
+		vim.cmd(":source %")
+	end
+end
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<C-h>", "<C-w>h>")
@@ -5,7 +10,7 @@ vim.keymap.set("n", "<C-j>", "<C-w>j>")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-f>", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader><CR>", ":source %<CR>")
+vim.keymap.set("n", "<leader><CR>", source_current_file)
 vim.keymap.set("n", "<leader>rh", ":Gitsigns reset_hunk<CR>")
 vim.keymap.set("n", "<leader>ph", ":Gitsigns preview_hunk<CR>")
 -- vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")

a61c2f786581a14bb6d58e18a9450461dfa13188
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-07-07 21:53:54 +0530

    added keymap for telescope grep_string picker

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fg", builtin.grep_string, {})
 vim.keymap.set(
 	"n",
 	"<C-b>",

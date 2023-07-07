9bce6af733340f24b15499383980bba60fd67097
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-07-07 23:08:37 +0530

    upgrade telescope to version 0.1.2

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -214,7 +214,7 @@ return require("packer").startup(function(use)
 	use("Shatur/neovim-ayu")
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
+		tag = "0.1.2",
 		-- or                            , branch = '0.1.x',
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})

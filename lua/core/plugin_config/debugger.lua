f33db12a66ef7698acd3a33bab2946307d0ff68d
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-07-23 14:29:31 +0530

    added nvim-dap nvim-dap-ui and nvim-dap-python

---
+++ lua/core/plugin_config/debugger.lua
@@ -0,0 +1,18 @@
+local dap, dapui = require("dap"), require("dapui")
+
+require("dap-python").setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+dapui.setup()
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+-- vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")

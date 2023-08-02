47b3dd1e4c891279e493d55ecec31b86501acdf8
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-08-02 22:02:22 +0530

    add default mappings for cmdline

--- lua/core/plugin_config/completion.lua
+++ lua/core/plugin_config/completion.lua
@@ -121,6 +121,7 @@ cmp.setup({
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
 		{ name = "path" },
 	}, {

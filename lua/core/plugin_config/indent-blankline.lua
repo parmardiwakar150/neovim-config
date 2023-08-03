34d7c29837280f5cf289e920707a8933ef62c302
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-08-03 14:56:33 +0530

    added keymap for moving to context end

--- lua/core/plugin_config/indent-blankline.lua
+++ lua/core/plugin_config/indent-blankline.lua
@@ -5,13 +5,25 @@ require("indent_blankline").setup({
 })
 
 vim.keymap.set("n", "[c", function()
-	local ok, start = require("indent_blankline.utils").get_current_context(
+	local ok, context_start = require("indent_blankline.utils").get_current_context(
 		vim.g.indent_blankline_context_patterns,
 		vim.g.indent_blankline_use_treesitter_scope
 	)
 	if not ok then
 		return
 	end
-	vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start, 0 })
+	vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { context_start, 0 })
+	vim.cmd([[normal! _]])
+end)
+
+vim.keymap.set("n", "]c", function()
+	local ok, _, context_end = require("indent_blankline.utils").get_current_context(
+		vim.g.indent_blankline_context_patterns,
+		vim.g.indent_blankline_use_treesitter_scope
+	)
+	if not ok then
+		return
+	end
+	vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { context_end, 0 })
 	vim.cmd([[normal! _]])
 end)

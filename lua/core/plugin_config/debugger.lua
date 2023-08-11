1a68491db141596c917ad03a03251a0447d5ee55
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-08-11 14:39:09 +0530

    added keymap for devdocs

--- lua/core/plugin_config/debugger.lua
+++ lua/core/plugin_config/debugger.lua
@@ -15,5 +15,5 @@ end
 
 vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
 vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-vim.keymap.set("n", "<leader>dd", ":DapContinue<CR>")
+vim.keymap.set("n", "<leader>db", ":DapContinue<CR>")
 -- vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")

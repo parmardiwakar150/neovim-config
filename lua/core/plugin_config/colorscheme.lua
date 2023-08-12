14d5e48923802e6e1ce8cc2543402623dcb8b0c5
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-08-12 22:07:20 +0530

    customize kanagawa theme and set it as the default theme

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -50,7 +50,7 @@ require("onedark").setup({
 		background = true, -- use background color for virtual text
 	},
 })
-vim.cmd([[ colorscheme onedark ]])
+-- vim.cmd([[ colorscheme onedark ]])
 -- vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#343e4f' })
 
 require("gruvbox").setup({
@@ -83,6 +83,73 @@ require("ayu").setup({
 	mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
 	overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
 })
+
+require("kanagawa").setup({
+	compile = false, -- enable compiling the colorscheme
+	undercurl = false, -- enable undercurls
+	commentStyle = { italic = true },
+	functionStyle = {},
+	keywordStyle = { italic = false },
+	statementStyle = { bold = false },
+	typeStyle = {},
+	transparent = false, -- do not set background color
+	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	colors = { -- add/modify theme and palette colors
+		palette = {},
+		theme = {
+			all = {
+				ui = {
+					bg_gutter = "none",
+				},
+			},
+		},
+	},
+	overrides = function(colors)
+		local theme = colors.theme
+		return {
+			NormalFloat = { bg = "none" },
+			FloatBorder = { bg = "none" },
+			FloatTitle = { bg = "none" },
+
+			Boolean = { bold = false },
+			["@text.uri"] = { underline = false },
+
+			-- Save an hlgroup with dark background and dimmed foreground
+			-- so that you can use it where your still want darker windows.
+			-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+			NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+			-- Popular plugins that open floats will link to NormalFloat by default;
+			-- set their background accordingly if you wish to keep them dark and borderless
+			-- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+			MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+			-- Telescope
+			-- TelescopeTitle = { fg = theme.ui.special, bold = true },
+			-- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+			-- TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+			-- TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+			-- TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+			-- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+			-- TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+			-- Completion menu
+			Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+			PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+			PmenuSbar = { bg = theme.ui.bg_m1 },
+			PmenuThumb = { bg = theme.ui.bg_p2 },
+		}
+	end,
+	theme = "wave", -- Load "wave" theme when 'background' option is not set
+	background = { -- map the value of 'background' option to a theme
+		dark = "wave", -- try "dragon" !
+		light = "lotus",
+	},
+})
+
+-- setup must be called before loading
+vim.cmd("colorscheme kanagawa")
 
 local opts = { noremap = true, silent = true }
 

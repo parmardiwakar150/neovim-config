0212f01cd044c298b9d798708ab0406322d05b11
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-08-23 19:59:12 +0530

    add parenthesis after nvim-cmp "confirm_done" event

--- lua/core/plugin_config/completion.lua
+++ lua/core/plugin_config/completion.lua
@@ -1,5 +1,6 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 local kind_icons = {
 	Array = "",
@@ -156,3 +157,5 @@ cmp.setup({
 		end,
 	},
 })
+
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())

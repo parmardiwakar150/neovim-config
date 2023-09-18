0d7be2edec9d2df8cac6eb2849fe32f9cf8fc0a2
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-09-18 23:23:39 +0530

    disable semantic tokens

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -21,3 +21,10 @@ vim.api.nvim_create_autocmd("bufWritePost", {
 	end,
 	group = format_on_save_group,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		client.server_capabilities.semanticTokensProvider = nil
+	end,
+})

f014ea8a9c7f1a7b8b2675e0a54c711b6dd2b78e
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2023-09-20 12:57:35 +0530

    don't show indent guide for blank lines

--- lua/core/plugin_config/indent-blankline.lua
+++ lua/core/plugin_config/indent-blankline.lua
@@ -1,6 +1,7 @@
 require("indent_blankline").setup({
 	-- for example, context is off by default, use this to turn it on
 	show_current_context = true,
+	show_trailing_blankline_indent = false,
 	-- show_current_context_start = true,
 })
 

922a0cbc99122c4b9681d7a30db78e2f3e794a1e
Diwakar Singh <parmardiwakar150@iitkgp.ac.in>
2024-07-17 19:15:58 +0530

    show relative file path in lualine

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -2,7 +2,7 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			"NvimTree",
@@ -19,13 +19,14 @@ require("lualine").setup({
 	},
 	sections = {
 		lualine_a = { { "mode", icon = "" } },
-		lualine_b = { { "branch", icon = "" }, "diff" },
+		lualine_b = { { "branch", icon = "" }, "diff" },
 		lualine_c = {
 			{
 				"filename",
 				symbols = {
 					modified = "●", -- Text to show when the buffer is modified
 				},
+				path = 1,
 			},
 		},
 		lualine_x = { "filetype" },
@@ -36,7 +37,7 @@ require("lualine").setup({
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "location" },
 		lualine_y = {},
 		lualine_z = {},
